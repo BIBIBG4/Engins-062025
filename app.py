8b5c341d6e3a0a9673c051ae03ca5861cb3d8fa7
BIBIBG4 <113338462+BIBIBG4@users.noreply.github.com>
2025-06-06 10:28:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routes import init_routes
 from db import init_db
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
+import os
 
 app = Flask(__name__)
 app.secret_key = "votre_clé_secrète"  # Change cette clé pour la sécurité
@@ -11,4 +12,5 @@ init_db()         # initialise la base au lancement
 init_routes(app)  # enregistre les routes
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
